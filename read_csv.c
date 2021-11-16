8cbc382cb275f6c16791940e5e5c1bcbc867729c
Audio431 <64627726+Audio431@users.noreply.github.com>
2021-11-16 14:39:30 +0700

    Add files via upload

---
+++ read_csv.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(){
+
+    char filename[100];
+    printf("Enter your .csv file location.: ");
+    fgets(filename, 15, stdin);
+    size_t len = strlen(filename);
+    if (len > 0 && filename[len - 1] == '\n'){
+    filename[len - 1] = '\0';
+    }
+
+    FILE* file = fopen(filename,"r");
+    if (file == NULL){
+        perror("Unable to open the file.");
+        exit(1);
+    }
+
+    char line[200];
+
+    while(fgets(line, sizeof(line), file)){
+        char * token;
+        
+        token = strtok(line, ",");
+
+        while(token != NULL){
+            printf("%-10s",token);
+            token = strtok(NULL, ",");
+        }
+
+        printf("\n");       
+    }
+
+}
