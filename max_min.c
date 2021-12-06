2e4ab3bc5379eb146b5980b7c99f717188f42d39
Audio431 <64627726+Audio431@users.noreply.github.com>
2021-12-06 21:14:44 +0700

    Add function to find maximum and minimum value

---
+++ max_min.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "get_each_csv_value.h"
+#include "count_row_col.h"
+
+double max_column(FILE*fp,int column){
+    size_t width = count_row("untitled.csv");
+    double max = 0;
+    for(size_t i = 0; i < width ; i++){  //get each value and store into string array
+        char data[100];
+        get_value_csv(fp,data,i+1,column);
+        double member = strtod(data,NULL);
+        if (member > max){
+            max = member;
+        }
+        else{
+            break;
+        }
+    }
+    return max;
+}
+    
+
+double min_column(FILE*fp,int column){
+    size_t width = count_row("untitled.csv");
+    double min = max_column(fp,column);
+    for(size_t i = 0; i < width ; i++){  //get each value and store into string array
+        char data[100];
+        get_value_csv(fp,data,i+1,column);
+        double member = strtod(data,NULL);
+        if (min > member){
+            min = member;
+        }
+    }
+    return min;
+}
