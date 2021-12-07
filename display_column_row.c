6049e34e32d33dff7312fef7dd6769cc846c41b8
Audio431 <64627726+Audio431@users.noreply.github.com>
2021-12-07 12:04:51 +0700

    Display duplicate value

--- display_column_row.c
+++ display_column_row.c
@@ -1,35 +1,27 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #include <ctype.h>
 #include "get_each_csv_value.h"
 #include "count_row_col.h"
 
-void display_col_row(char *value){
-    
-    FILE*fp = fopen("untitled.csv","r");
-    size_t width = count_row(fp);
-    size_t length = count_column(fp);
-    size_t i;
+void display_col_row(FILE*fp,char *value){
+    size_t width = count_row(fp)+1;
+    size_t length = count_column(fp)+1;
     int row = -1;
     int col = -1;
-    for (i = 0; i < width+1; i++)
-    {
-        for (size_t j = 0; j < length+1; j++)
-        {
-            char buff[100];
+    for (size_t i = 0; i < width; i++){
+        for (size_t j = 0; j < length; j++){
+            char*buff = (char*)malloc(sizeof(char)*100);
             get_value_csv(fp,buff,i,j);
-            if (strcmp(value,buff) == 0) {
+            if (strcmp(value,buff) == 0){
                 row = i;
                 col = j;
-                break;
+                printf("%s [row = %d col = %d]\n",value,row,col);
             }
         } 
     }
     if (row == -1 || col == -1){
         printf("Value is not found.");
     }
-    else {
-        printf("%s [row = %d col = %d]",value,row,col);
-    }
 }
-
