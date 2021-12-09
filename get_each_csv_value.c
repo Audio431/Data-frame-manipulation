cb7cd3cae22fc13ab749ee9f61ace65bcf69f9a0
Audio431 <64627726+Audio431@users.noreply.github.com>
2021-12-09 16:42:14 +0700

    Bug fixed

--- get_each_csv_value.c
+++ get_each_csv_value.c
@@ -4,8 +4,6 @@
 #include <ctype.h>
 #include "count_row_col.h"
 
-
-
 void slice_str(const char * str, char * buffer, size_t start, size_t end) //function to slice string
 {
     if (start > end){
@@ -15,11 +13,12 @@ void slice_str(const char * str, char * buffer, size_t start, size_t end) //func
     for ( size_t i = start; i <= end; ++i ) {
         buffer[j++] = str[i];
     }
-    buffer[j] = 0;
+    buffer[j] = '\0';
 }
 
 void get_value_csv(FILE*fp,char * buff,int row, int column ){
-
+    
+    fseek(fp,0,SEEK_SET);
     fseek(fp,0,SEEK_END);
     char *table = malloc(ftell(fp));
     fseek(fp,0,SEEK_SET); //set pointer file to the beginning
@@ -29,14 +28,14 @@ void get_value_csv(FILE*fp,char * buff,int row, int column ){
         if (c == ',' || c ==  '\n'){
            index_count++;  
         } 
-        table[i++] =  c ;
+        table[i++] =  c;
     }
+    table[i] = '\0';
 
-    
-    int index_delim[index_count]; 
+    int index_delim[index_count];
     size_t j = 0;
     for ( i = 0 ; i < strlen(table) ; i++){             //get index of delim in .csv and store to array
-        if (table[i] ==  ',' || table[i] == '\n'){  
+        if (table[i] ==  ',' || table[i] == '\n' || table[i] == '\0'){  
             index_delim[j++] = i; 
         }
     }
@@ -57,4 +56,3 @@ void get_value_csv(FILE*fp,char * buff,int row, int column ){
     }
     free(table);
 }
-
